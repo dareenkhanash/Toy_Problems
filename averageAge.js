31101550bf224459ef9ffbd0a02a0dd1b8460862
dareenkhanash <dareenhanash1988@gmail.com>
2018-02-19 09:45:14 +0200

    average  Dareen

--- averageAge.js
+++ averageAge.js
@@ -12,6 +12,37 @@
  ]; 
  function averageAge(people) { 
        // YOUR CODE HERE 
+       var counter=0
+       var sum=reduce(people,function(acc,element,i){
+       		if(element.age>=18 && element.age<=50){
+       			acc=acc+element.age
+       			counter++
+
+       		}
+       		return acc
+       },0)
+       return sum/counter
  }
 
  averageAge(people); // 43+36+44+24 = 36.74
+ function reduce(array, f, acc) { 
+       if (acc === undefined) { 
+             acc = array[0]; 
+             array = array.slice(1); 
+       } 
+       each(array, function(element, i) { 
+             acc = f(acc, element, i); 
+       }); 
+       return acc; 
+ }
+  function each(coll, func) { 
+       if (Array.isArray(coll)) { 
+             for (var i = 0; i < coll.length; i++) { 
+                   func(coll[i], i); 
+             } 
+       } else { 
+             for (var key in coll) { 
+                   func(coll[key], key); 
+             } 
+       } 
+ }
