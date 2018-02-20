9d2b7cf2018c604ed58b216521c1ff793c902c3e
dareenkhanash <dareenhanash1988@gmail.com>
2018-02-20 09:27:40 +0200

    dareen oop

--- Oop.js
+++ Oop.js
@@ -7,24 +7,27 @@ function randInt(n) {
 }
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+  var instance={}
+  instance.upper=upperbound;
+  instance.rand=randInt(instance.upper)
+  instance.counter=0;
+  instance.guessMyNumber=guessMyNumber
+  instance.giveUp=giveUp
+  instance.numOfGuesses=numOfGuesses
+  return instance
 }
+var guessMyNumber=function(n){
+      this.counter++;
+      if (n > this.upper) {
+        return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    }
+var giveUp=function(){
+      return this.rand;
+    }
+var numOfGuesses=function(){
+      return this.counter;
+    }
