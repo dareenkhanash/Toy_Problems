05f68273fb83a328736fd855e115182d6eccef7b
dareenkhanash <dareenhanash1988@gmail.com>
2018-02-27 09:53:37 +0200

    accum

--- accum.js
+++ accum.js
@@ -4,6 +4,42 @@
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
+function reduce(array, f, acc) { 
+       if (acc === undefined) { 
+             acc = array[0]; 
+             array = array.slice(1); 
+       } 
+       each(array, function(element, i) { 
+             acc = f(acc, element, i); 
+       }); 
+       return acc; 
+ }
+  function each(coll, f) { 
+       if (Array.isArray(coll)) { 
+             for (var i = 0; i < coll.length; i++) { 
+                   f(coll[i], i); 
+             } 
+       } else { 
+             for (var key in coll) { 
+                   f(coll[key], key); 
+             } 
+       } 
+ } 
+
+	
     function accum(input){
         //your code is here
+       return reduce(input, function(acc,element,i){
+       	for(var x=0;x<i+1;x++){
+       		if (x===0){
+       			acc+=element.toUpperCase()
+       		}else{
+       			acc+=element
+       		}
+       		if(x===i){
+			acc+="-"
+       		}
+       	}
+       	return acc
+       },"") 
     }
